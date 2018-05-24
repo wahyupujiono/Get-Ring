9e27d59070a95eb0b6f7ee02d8e78e1cebb17197
wahyupujiono <pujiono_wahyu@yahoo.co.id>
2018-05-24 11:54:27 +0700

    update bugs

--- resources/views/customer/_details.blade.php
+++ resources/views/customer/_details.blade.php
@@ -18,7 +18,7 @@
             <td data-th="Subtotal" class="text-right" colspan="2">Rp{{number_format($detail->total_price)}}</td>
         </tr>
         @endforeach
-<!--        <tr>
+<!--        <tr> 
             <td data-th="Subtotal"><strong>Biaya Administrasi</strong></td>
             <td data-th="Subtotal" class="text-right" colspan="2">Rp{{ number_format($detail->total_fee) }}</td>
         </tr>-->
